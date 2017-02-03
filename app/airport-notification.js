2a4649e2717c81137622d551a3c44a137a948e6b
Adriana Castro <castrocadriana@gmail.com>
2017-02-02 23:31:52 -0500

    slackbot added

--- app/airport-notification.js
+++ app/airport-notification.js
@@ -1,4 +1,4 @@
-/*
+  /*
  * This requires: restler
  * To install, type 'npm install restler'
  * Tested with node.js v0.6.14
@@ -6,10 +6,35 @@
 
 var util = require('util');
 var restclient = require('restler');
-
 var fxml_url = 'http://flightxml.flightaware.com/json/FlightXML2/';
 var username = 'YOUR_USERNAME';
 var apiKey = 'YOUR_APIKEY';
+
+var Botkit = require('botkit');
+ 
+var controller = Botkit.slackbot();
+ 
+var bot = controller.spawn({
+ 
+  token: "xoxb-135760442560-U6BWZ51Z4XRXMi5rQNnKW7da"
+ 
+})
+ 
+bot.startRTM(function(err,bot,payload) {
+ 
+  if (err) {
+ 
+    throw new Error('Could not connect to Slack');
+ 
+  }
+ 
+});
+ 
+controller.hears(["Hello","Hi"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+ 
+  bot.reply(message,'Hello, how are you today?');
+ 
+});
 
 
 restclient.get(fxml_url + 'MetarEx', {
